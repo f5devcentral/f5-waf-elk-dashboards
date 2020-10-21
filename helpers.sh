bb5ff6f7ed4c0152451f5bb02fda59cd58632144
Mikhail Fedorov <m.fedorov@f5.com>
2020-10-21 16:45:35 -0700

    remove comments

--- helpers.sh
+++ helpers.sh
@@ -10,5 +10,3 @@ json2ndjson () {
               | if .attributes.kibanaSavedObjectMeta.searchSourceJSON then .attributes.kibanaSavedObjectMeta.searchSourceJSON|=tojson else . end
               | if .attributes.panelsJSON then .attributes.panelsJSON|=tojson else . end'
 }
-# cat kibana/overview-dashboard.ndjson | ndjson2json > kibana/overview-dashboard.json
-# cat kibana/overview-dashboard.json | json2ndjson > kibana/overview-dashboard.ndjson
